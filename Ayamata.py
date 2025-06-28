09f097824b70acf3a49f17ab3d945b9d95a191eb
Mathy <65187882+Mistycy@users.noreply.github.com>
2025-06-28 11:08:58 -0300

    Update Ayamata.py
    Scan a list of a ports and Error Detection.

--- Ayamata.py
+++ Ayamata.py
@@ -1,18 +1,20 @@
 import socket
-
+import time
+ports = [21, 22, 23, 25, 80, 110, 139, 443, 445, 3389]
 def port_scanner():
-        try:
-                host = str(input("Host:  "))
-                port = int(input("Port:  "))
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(1)
-                if s.connect_ex((host, port)) == 0:
-                        print(f"{host}:  port {port}  open")
-                        s.close()
-                else:
-                        print("port closed")
-        except:
-                print("error")
+	try:
+		host = str(input("Host:  "))
+		#port = int(input("Port:  "))
+		for port in ports:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.settimeout(1)
+			time.sleep(2)
+			if s.connect_ex((host, port)) == 0:
+				print(f"Host: {host} | Port {port} | Open")
+				s.close()
+			else:
+				print(f"Host: {host} | Port {port} | Closed")
 
+	except Exception as e:
+		print(f"error:  {e}")
 port_scanner()
-
