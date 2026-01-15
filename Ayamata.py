22e941866c918c03c087efef240f85f4481d014c
Mathy <matheushenriquealcantara47@gmail.com>
2026-01-15 00:39:34 -0300

    Ayamata 0.2

--- Ayamata.py
+++ Ayamata.py
@@ -6,8 +6,11 @@ import time
 import argparse
 from colorama import Fore
 #color line
+
+top_ports = [20, 21, 22, 23, 25, 53, 80, 110, 119, 123, 143, 161, 194, 443]
+
 def simple_port_scanner():
-	try:
+        try:
 #create the object on the "parser".
                 blue = Fore.BLUE
                 white = Fore.WHITE
@@ -18,20 +21,33 @@ def simple_port_scanner():
                 parser = argparse.ArgumentParser(description="port scanner", add_help=False)
                 parser.add_argument("--host", type=str, help="define the host")
                 parser.add_argument("--port", type=int, nargs="*", help="define the port")
+                parser.add_argument("--top_ports", action="store_true")
 
 		#args object
                 args = parser.parse_args()
 
-                for port in args.port:
-                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        s.settimeout(1)
-                        time.sleep(2)
-                        if s.connect_ex((args.host, port)) == 0:
-                                print(f"{white}Host: {yellow}{args.host} {white}| Port: {yellow}{port} {white}| {green}Open")
-                                s.close()
-                        else:
-                                print(f"{white}Host: {yellow}{args.host} {white}| Port: {yellow}{port} {white}| {red}Closed")
-	except Exception as e:
-		print(f"error:  {e}")
+                if args.top_ports:
+                        for port in top_ports:
+                                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                s.settimeout(1)
+                                time.sleep(2)
+                                if s.connect_ex((args.host, port)) == 0:
+                                        print(f"{white}Host: {yellow}{args.host} {white}| Port: {yellow}{port} {white}| {green}Open")
+                                        s.close()
+                                else:
+                                        print(f"{white}Host: {yellow}{args.host} {white}| Port: {yellow}{port} {white}| {red}Closed")
+                                        
+                if args.port:
+                        for port in args.port:
+                                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                s.settimeout(1)
+                                time.sleep(2)
+                                if s.connect_ex((args.host, port)) == 0:
+                                        print(f"{white}Host: {yellow}{args.host} {white}| Port: {yellow}{port} {white}| {green}Open")
+                                        s.close()
+                                else:
+                                        print(f"{white}Host: {yellow}{args.host} {white}| Port: {yellow}{port} {white}| {red}Closed")
+        except Exception as e:
+                print(f"error:  {e}")
 simple_port_scanner()
 
